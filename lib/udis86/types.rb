1c2f9defd792376b2248ed039be86ff2e845170a
postmodern <postmodern.mod3@gmail.com>
2009-05-21 19:56:28 -0700

    Use FFI's enum method.

--- lib/udis86/types.rb
+++ lib/udis86/types.rb
@@ -4,20 +4,13 @@ module FFI
   module UDis86
     extend FFI::Library
 
-    # 16-bit disassembly mode
-    MODE_16 = 16
+    enum :ud_mode, [
+      :ud_mode_16, 16,
+      :ud_mode_32, 32,
+      :ud_mode_64, 64
+    ]
 
-    # 32-bit disassembly mode
-    MODE_32 = 32
-
-    # 64-bit disassembly mode
-    MODE_64 = 64
-
-    # Vendors
-    VENDORS = {
-      :amd => 0,
-      :intel => 1
-    }
+    enum :ud_vendors, [:amd, :intel]
 
     # Syntaxes
     SYNTAX = {
