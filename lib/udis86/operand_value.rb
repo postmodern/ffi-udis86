211514a61059aeccf98621b24213ac35c30444aa
postmodern <postmodern.mod3@gmail.com>
2009-11-23 07:55:27 -0800

    Added YARD tags to UDis86::OperandValue.

--- lib/udis86/operand_value.rb
+++ lib/udis86/operand_value.rb
@@ -16,54 +16,108 @@ module FFI
              :uqword, :uint64,
              :ptr, OperandPointer
 
+      #
+      # The signed byte value of the operand.
+      #
+      # @return [Integer]
+      #   The signed byte value.
+      #
       def sbyte
         self[:sbyte]
       end
 
       alias signed_byte sbyte
 
+      #
+      # The unsigned byte value of the operand.
+      #
+      # @return [Integer]
+      #   The unsigned byte value.
+      #
       def ubyte
         self[:ubyte]
       end
 
       alias unsigned_byte ubyte
 
+      #
+      # The signed word value of the operand.
+      #
+      # @return [Integer]
+      #   The signed word value.
+      #
       def sword
         self[:sword]
       end
 
       alias signed_word sword
 
+      #
+      # The unsigned word value of the operand.
+      #
+      # @return [Integer]
+      #   The unsigned word value.
+      #
       def uword
         self[:uword]
       end
 
       alias unsigned_word uword
 
+      #
+      # The signed double-word value of the operand.
+      #
+      # @return [Integer]
+      #   The signed double-word value.
+      #
       def sdword
         self[:sdword]
       end
 
       alias signed_double_word sdword
 
+      #
+      # The unsigned double-word value of the operand.
+      #
+      # @return [Integer]
+      #   The unsigned double-word value.
+      #
       def udword
         self[:udword]
       end
 
       alias unsigned_double_word udword
 
+      #
+      # The signed quad-word value of the operand.
+      #
+      # @return [Integer]
+      #   The signed quad-word value.
+      #
       def sqword
         self[:sqword]
       end
 
       alias signed_quad_word sqword
 
+      #
+      # The unsigned quad-word value of the operand.
+      #
+      # @return [Integer]
+      #   The unsigned quad-word value.
+      #
       def uqword
         self[:uqword]
       end
 
       alias unsigned_quad_word uqword
 
+      #
+      # The pointer value of the operand.
+      #
+      # @return [OperandPointer]
+      #   The pointer value.
+      #
       def ptr
         self[:ptr]
       end
