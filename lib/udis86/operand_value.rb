813a9807a16f1c2f5d84ef4ca032659d63f07d6a
postmodern <postmodern.mod3@gmail.com>
2009-11-23 07:40:47 -0800

    Added instance methods to UDis86::OperandValue.

--- lib/udis86/operand_value.rb
+++ lib/udis86/operand_value.rb
@@ -16,6 +16,60 @@ module FFI
              :uqword, :uint64,
              :ptr, OperandPointer
 
+      def sbyte
+        self[:sbyte]
+      end
+
+      alias signed_byte sbyte
+
+      def ubyte
+        self[:ubyte]
+      end
+
+      alias unsigned_byte ubyte
+
+      def sword
+        self[:sword]
+      end
+
+      alias signed_word sword
+
+      def uword
+        self[:uword]
+      end
+
+      alias unsigned_word uword
+
+      def sdword
+        self[:sdword]
+      end
+
+      alias signed_double_word sdword
+
+      def udword
+        self[:udword]
+      end
+
+      alias unsigned_double_word udword
+
+      def sqword
+        self[:sqword]
+      end
+
+      alias signed_quad_word sqword
+
+      def uqword
+        self[:uqword]
+      end
+
+      alias unsigned_quad_word uqword
+
+      def ptr
+        self[:ptr]
+      end
+
+      alias pointer ptr
+
     end
   end
 end
