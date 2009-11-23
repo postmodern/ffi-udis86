25e7d3e380ab2e4536dd799096473c9f49b251da
postmodern <postmodern.mod3@gmail.com>
2009-11-23 07:52:00 -0800

    Added YARD tags to UDis86::Operand.

--- lib/udis86/operand.rb
+++ lib/udis86/operand.rb
@@ -15,30 +15,72 @@ module FFI
              :scale, :uint8
 
 
+      #
+      # Returns the type of the operand.
+      #
+      # @return [Integer]
+      #   The type of the operand.
+      #
       def type
         self[:type]
       end
 
+      #
+      # Returns the size of the operand.
+      #
+      # @return [Integer]
+      #   The size of the operand in bytes.
+      #
       def size
         self[:size]
       end
 
+      #
+      # The value of the operand.
+      #
+      # @return [OperandValue]
+      #   The value of the operand.
+      #
       def value
         self[:value[
       end
 
+      #
+      # Returns the base address used by the operand.
+      #
+      # @return [Integer]
+      #   The base address of the operand.
+      #
       def base
         self[:base]
       end
 
+      #
+      # Returns the index value used by the operand.
+      #
+      # @return [Integer]
+      #   The index value of the operand.
+      #
       def index
         self[:index]
       end
 
+      #
+      # Returns the offset value used by the operand.
+      #
+      # @return [Integer]
+      #   The offset value of the operand.
+      #
       def offset
         self[:offset]
       end
 
+      #
+      # Returns the scale value used by the operand.
+      #
+      # @return [Integer]
+      #   The scale value of the operand.
+      #
       def scale
         self[:scale]
       end
