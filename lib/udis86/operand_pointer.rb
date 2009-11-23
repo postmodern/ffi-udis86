75e9ecee758954a9322426799bf23a1106efb0c5
postmodern <postmodern.mod3@gmail.com>
2009-11-23 07:58:17 -0800

    Added YARD tags to UDis86::OperandPointer.

--- lib/udis86/operand_pointer.rb
+++ lib/udis86/operand_pointer.rb
@@ -7,12 +7,24 @@ module FFI
       layout :seg, :uint16,
              :off, :uint32
 
+      #
+      # Returns the pointer segment.
+      #
+      # @return [Integer]
+      #   The pointer segment.
+      #
       def seg
         self[:seg]
       end
 
       alias segment seg
 
+      #
+      # Returns the offset within the segment of the pointer.
+      #
+      # @return [Integer]
+      #   The offset within the segment.
+      #
       def off
         self[:off]
       end
