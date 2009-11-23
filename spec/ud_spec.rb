5537e0a8ffc619f10ded6f3d4f82b16ad4576982
postmodern <postmodern.mod3@gmail.com>
2009-11-23 14:33:07 -0800

    Added a few specs for UDis86::UD.

---
+++ spec/ud_spec.rb
@@ -0,0 +1,33 @@
+require 'udis86/ud'
+
+require 'spec_helper'
+
+describe UD do
+  describe "create" do
+    it "should accept a :mode option" do
+      ud = UD.create(:mode => 16)
+      ud.mode.should == 16
+    end
+
+    it "should accept a :syntax option" do
+      lambda {
+        UD.create(:syntax => :att)
+      }.should_not raise_error(ArgumentError)
+    end
+
+    it "should accept a :vendor option" do
+      ud = UD.create(:vendor => :amd)
+      ud.vendor.should == :amd
+    end
+
+    it "should accept a :pc option" do
+      ud = UD.create(:pc => 0x400000)
+      ud.pc.should == 0x400000
+    end
+
+    it "should accept a :string option" do
+      ud = UD.create(:string => "\x90\x90\x90")
+      ud.input_buffer.should == "\x90\x90\x90"
+    end
+  end
+end
