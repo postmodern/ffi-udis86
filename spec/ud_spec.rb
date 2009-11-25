1470903e44fad5ade5d86907c77b576d57e86df0
postmodern <postmodern.mod3@gmail.com>
2009-11-25 03:48:47 -0800

    Added more specs for UDis86::UD.

--- spec/ud_spec.rb
+++ spec/ud_spec.rb
@@ -45,12 +45,65 @@ describe UD do
     end
   end
 
-  it "should be able to open files" do
-    UD.open(File.join(Helpers::FILES_DIR,'simple.o')) do |ud|
-      ud.next_insn.should == 1
-      ud.to_hex.should == '90'
+  describe "open" do
+    it "should be able to open files" do
+      UD.open(File.join(Helpers::FILES_DIR,'simple.o')) do |ud|
+        ud.next_insn.should == 1
+        ud.to_hex.should == '90'
 
-      ud.next_insn.should == 0
+        ud.next_insn.should == 0
+      end
+    end
+  end
+
+  describe "disassember" do
+    before(:each) do
+      @string = "\x90\x90\xc3"
+      @ud = UD.create(:string => @string)
+    end
+
+    it "should provide read access to the input buffer" do
+      @ud.input_buffer.should == @string
+    end
+
+    it "should allow setting the input buffer" do
+      new_input = "\xc3"
+
+      @ud.input_buffer = new_input
+      @ud.input_buffer.should == new_input
+    end
+
+    it "should allow setting an input callback" do
+      bytes = [0x90, 0xc3]
+
+      @ud.input_callback do |ud|
+        bytes.shift || -1
+      end
+
+      @ud.next_insn.should == 1
+      @ud.to_asm.should == 'nop '
+
+      @ud.next_insn.should == 1
+      @ud.to_asm.should == 'ret '
+
+      @ud.next_insn.should == 0
+    end
+
+    it "should allow the skipping of input bytes" do
+      @ud.skip(2)
+
+      @ud.next_insn
+      @ud.to_asm.should == 'ret '
+    end
+
+    it "should provide hex form of the bytes" do
+      @ud.next_insn.should == 1
+      @ud.to_hex.should == '90'
+    end
+
+    it "should provide the assembly form of the disassembled instructions" do
+      @ud.next_insn.should == 1
+      @ud.to_asm.should == 'nop '
     end
   end
 end
