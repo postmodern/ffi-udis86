a4449d3cc3fcc668ffc3bc14afe09852fe1a0299
postmodern <postmodern.mod3@gmail.com>
2009-11-27 08:46:32 -0800

    Fixed the helpers.

--- spec/helpers/files.rb
+++ spec/helpers/files.rb
@@ -1,9 +1,11 @@
 require 'udis86/ud'
 
+require 'spec_helper'
+
 module Helpers
   FILES_DIR = File.expand_path(File.join(File.dirname(__FILE__),'files'))
 
-  def ud_file(name)
-    UD.open(File.join(FILES_DIR,"#{name}.o"))
+  def ud_file(name,&block)
+    UD.open(File.join(FILES_DIR,"#{name}.o"),&block)
   end
 end
--- spec/helpers/operands.rb
+++ spec/helpers/operands.rb
@@ -1,12 +1,14 @@
 require 'udis86/ud'
 
+require 'spec_helper'
 require 'helpers/files'
 
 module Helpers
-  def ud_operands(file)
-    ud = ud_file(file)
-    ud.next_insn
+  def ud_operands(name)
+    ud_file(name) do |ud|
+      ud.next_insn
 
-    return ud.operands
+      return ud.operands
+    end
   end
 end
