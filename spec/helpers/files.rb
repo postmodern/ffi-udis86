d2136735360c3e3fc3d73e49e32663eae6c80b0d
postmodern <postmodern.mod3@gmail.com>
2009-11-27 08:38:30 -0800

    Append .o to all object file names.

--- spec/helpers/files.rb
+++ spec/helpers/files.rb
@@ -4,6 +4,6 @@ module Helpers
   FILES_DIR = File.expand_path(File.join(File.dirname(__FILE__),'files'))
 
   def ud_file(name)
-    UD.open(File.join(FILES_DIR,name.to_s))
+    UD.open(File.join(FILES_DIR,"#{name}.o"))
   end
 end
