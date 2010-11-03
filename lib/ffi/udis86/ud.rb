f14323275c27146de156fb94c0af64b2cf7b7691
Postmodern <postmodern.mod3@gmail.com>
2010-11-03 14:02:42 -0700

    Revert "Simplify UD#input_callback."
    * The UD.create must be passed back the actual UD object, not an
      FFI::Pointer.

--- lib/ffi/udis86/ud.rb
+++ lib/ffi/udis86/ud.rb
@@ -227,7 +227,7 @@ module FFI
       #
       def input_callback(&block)
         if block
-          @input_callback = Proc.new(&block)
+          @input_callback = Proc.new { |ptr| block.call(self) }
 
           UDis86.ud_set_input_hook(self,@input_callback)
         end
