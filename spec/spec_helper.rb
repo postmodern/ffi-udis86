c42ccccf486035cfeb130c5aac901db15f0f008e
Postmodern <postmodern.mod3@gmail.com>
2010-11-03 13:45:56 -0700

    Removed Bundler from spec/spec_helper.rb.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,5 @@
-require 'rubygems'
-require 'bundler'
-
-begin
-  Bundler.setup(:runtime, :test)
-rescue Bundler::BundlerError => e
-  STDERR.puts e.message
-  STDERR.puts "Run `bundle install` to install missing gems"
-  exit e.status_code
-end
-
-require 'spec'
+gem 'rspec', '~> 2.0.0'
+require 'rspec'
 require 'ffi/udis86/version'
 
 include FFI
