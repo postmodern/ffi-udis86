3d344416f1a9242a332bc62cdf28d1521e3fdf50
Postmodern <postmodern.mod3@gmail.com>
2020-02-15 22:12:17 -0800

    Fixed FFI::UDis86 specs.

--- spec/udis86_spec.rb
+++ spec/udis86_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
 
-require 'ffi/udis86/version'
+require 'ffi/udis86'
 
-describe UDis86 do
+describe FFI::UDis86 do
   it "should have a VERSION constant" do
-    expect(UDis86.const_defined?('VERSION')).to eq(true)
+    expect(described_class.const_defined?('VERSION')).to eq(true)
   end
 
   describe "types" do
@@ -14,9 +14,9 @@ describe UDis86 do
     end
 
     it "should define mappings from :ud_type to register names" do
-      ud_type = UDis86.enum_type(:ud_type)
+      ud_type = described_class.enum_type(:ud_type)
 
-      UDis86::REGS.each do |type,name|
+      described_class::REGS.each do |type,name|
         expect(:"ud_r_#{name}").to eq(type)
         expect(ud_type[ud_type[type]]).to eq(type)
       end
